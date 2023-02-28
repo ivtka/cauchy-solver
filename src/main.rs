d1d1044ba6efb8ef4ccec0d8488b39ecbc06860e
seshotake <seshotake@gmail.com>
2023-02-28 20:42:00 +0200

    add iterate method

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 mod derive;
 
-use derive::find_optimal_h;
-use derive::Scheme;
+use derive::*;
 
 fn main() {
     let eps = 1e-2;
@@ -28,17 +27,39 @@ fn main() {
         (x_next, y_next)
     };
 
-    let ng4 = |values: &Vec<f64>, i: usize, h: f64, t: f64, x_s: f64, y_s: f64| {
-        48.0 / 25.0 * values[i - 4] - 36.0 / 25.0 * values[i - 3] + 16.0 / 25.0 * values[i - 2]
-            - 3.0 / 25.0 * values[i - 1]
-            + 12.0 / 25.0 * h * dx(t, x_s, y_s)
+    let ng4 = |values: &Vec<f64>, i: usize, h: f64, t: f64, x_s: f64, y_s: f64, f: DeriveFn| match f
+    {
+        DeriveFn::Dx => {
+            48.0 / 25.0 * values[i - 4] - 36.0 / 25.0 * values[i - 3] + 16.0 / 25.0 * values[i - 2]
+                - 3.0 / 25.0 * values[i - 1]
+                + 12.0 / 25.0 * h * dx(t, x_s, y_s)
+        }
+        DeriveFn::Dy => {
+            48.0 / 25.0 * values[i - 4] - 36.0 / 25.0 * values[i - 3] + 16.0 / 25.0 * values[i - 2]
+                - 3.0 / 25.0 * values[i - 1]
+                + 12.0 / 25.0 * h * dy(t, x_s, y_s)
+        }
     };
     let optimal_h = find_optimal_h(x0, y0, t0, h0, eps, yrk21);
 
     let num = (1.0 / optimal_h + 1.0) as usize;
 
     let mut scheme = Scheme::new(x0, y0, optimal_h, yrk21, 3, num);
-    scheme.newton(optimal_h, eps, ng4);
+    scheme.newton(optimal_h, eps, ng4, 4);
+
+    let mut i = 0;
+
+    while (t0 - 1.0).abs() > eps {
+        let (x_t, y_t) = (t0.powi(2).exp() / 2.0, (-t0.powi(2).exp() / 2.0));
+
+        println!("({}, {}) - ({x_t}, {y_t})", scheme.0[i], scheme.1[i]);
+
+        t0 += optimal_h;
+        i += 1;
+    }
+
+    scheme = Scheme::new(x0, y0, optimal_h, yrk21, 3, num);
+    scheme.iterate(optimal_h, eps, ng4, 4);
 
     let mut i = 0;
 
